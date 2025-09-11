d7a94cb5824925d688b4437f0e8aef60a000b0d2
Gustavo <gustavomanfredi1995@gmail.com>
2025-09-11 00:12:34 -0300

    mejorar logica de script.js

---
+++ U13/cotizador-hogar/js/script.js
@@ -0,0 +1,14 @@
+const costoBasexm2 = 1.16;
+let totalm2 = Number(prompt("Ingrese los metros cuadrados de la propiedad:"));
+
+if (!isNaN(totalm2)) {
+    let fm = 1.07;
+    alert("El costo total de la propiedad es: $" + costoTotal.toFixed(2));
+    console.log("El valor ingresado es un número");
+} else {
+    console.log("El valor ingresado no es un número");
+    alert("Por favor ingrese un número válido mayor a 0");
+
+}
+
+
