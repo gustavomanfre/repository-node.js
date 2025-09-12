3efbe05a8c6162ad087c4615ef58432c09641213
Gustavo <gustavomanfredi1995@gmail.com>
2025-09-11 23:41:30 -0300

    mejorar logica de script.v1

--- U13/cotizador-hogar/js/script.js
+++ U13/cotizador-hogar/js/script.js
@@ -1,14 +1,25 @@
-const costoBasexm2 = 1.16;
+// Las constantes por convención se escriben en MAYÚSCULAS.
+const COSTO_BASE_M2 = 1.16;
+
+// La función constructora Number() castea/convierte el valor del prompt a tipo número.
+// Si la conversión falla, retorna NaN
 let totalm2 = Number(prompt("Ingrese los metros cuadrados de la propiedad:"));
 
-if (!isNaN(totalm2)) {
-    let fm = 1.07;
-    alert("El costo total de la propiedad es: $" + costoTotal.toFixed(2));
-    console.log("El valor ingresado es un número");
+//Las variables declaradas en el ambito global son accesibles desde cualquier parte del código tanto bloque o funcion. 
+let fm = 1.07;
+
+// isNaN() verifica si el valor NO es un número (Not a Number), devuelve true si NO es un número o false si es un número
+if (!isNaN(totalm2 && totalm2 > 0)) {
+    // toFixed(2) formatea el número a 2 decimales.
+    alert("El costo total de la propiedad es: $" + (COSTO_BASE_M2*totalm2*fm).toFixed(2));
 } else {
     console.log("El valor ingresado no es un número");
     alert("Por favor ingrese un número válido mayor a 0");
-
 }
 
+//Las variables let/const tienen ambito de bloque, es decir solo son accesibles dentro del bloque donde fueron declaradas.
+//Las variables var tienen ambito de función, es decir son accesibles dentro de la función donde fueron declaradas.
 
+
+
+                                                                                                                                                                                           
